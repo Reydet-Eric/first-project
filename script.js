ee2974e1b24694b83669f84883e036c8c9eb65d2
Reydet-Eric <eric.reydet@sncf.fr>
2022-11-29 16:53:58 +0100

    first try of js

---
+++ script.js
@@ -0,0 +1,48 @@
+/*structure de mon objet
+const week={name: "week1",
+notion:[["notion 1",img],["notion2",img]],
+ressenti:"blabla"}
+*/
+
+/*structure du menu
+const menu={name:"menu1",
+lien:href="#week1",
+actif:"oui ou non"}
+*/
+
+//création du formulaire d'ajout de semaine
+
+function addWeek(){
+console.log("formulaire")
+//variables nécessaires
+
+//fonction ajoutWeek
+
+//afficher le bloc content
+
+//afficher le titre
+
+    //afficher "week"+1
+
+//afficher le bloc notion
+
+    // combien de notion?
+
+        //nom de la notion
+
+        //ajout d'une photo
+
+//afficher le bloc ressenti
+
+    // saisie du ressenti
+
+//valider le formulaire en stockant les infos
+
+}
+// appeler la fonction nouvelle semaine
+const clickMenuAdd=document.querySelector('#add');
+
+clickMenuAdd.addEventListener('click',eventMenu => {
+    console.log("ouverture formulaire d'ajout de semaine");
+    addWeek();
+});
