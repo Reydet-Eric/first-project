106de5b0c753c00d1153cb4740f8160e8d99a009
Reydet-Eric <eric.reydet@sncf.fr>
2022-12-01 16:49:16 +0100

    add button menu

--- script.js
+++ script.js
@@ -2,14 +2,13 @@
 const week={name: [1,2,3],
 notion:[["Apprendre à apprendre\nC'est quoi être développeur?"],["Base: la console sous linux","GIT /  GITHUB","une IDE : VSCode"]],
 ressenti:["Après avoir attendu plusieurs mois, j'y suis enfin !!!! \n","Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed molestiae alias ut id assumenda quas iusto laborum eveniet harum laudantium, eius consectetur, ducimus, nulla accusantium ipsam hic eaque architecto autem."]}
+
 const numberWeek=week.name.length
 //console.log(`${week.name} ${week.notion} ${week.ressenti}`)
 //structure du menu
-const menu={name:"menu1",
-lien:href="#week1",
-actif:false}
-// affichage menu
-
+// const menu={name:"menu1",
+// lien:href="#week1",
+// actif:false}
 
 // affichage des bulles de contenu
 
@@ -17,7 +16,7 @@ const rightSection=document.querySelector('.right');
 const contentSection=document.querySelector('.content');
 
 function affich(){
-    console.log("affichage des cards");
+    
 for(let i=0;i<week.notion.length;i++){
  //affichage bloc content
 newContentSection=document.createElement('section');
@@ -31,7 +30,7 @@ newContentSection.appendChild(newSectionCard);
 
     //  affichage titre semaine
  newContentH2=document.createElement('h2');
- newContentH2.id=`week i+1`;
+ newContentH2.setAttribute("id",`#week${i+1}`);
  newSectionCard.appendChild(newContentH2);
  newContentH2.innerHTML=`week ${i+1}`;
     //  affichage bulle notion
@@ -41,18 +40,19 @@ divDeNotion.classList.add('notion');
 newSectionCard.appendChild(divNotion);
 divNotion.innerHTML=`${week.notion[i]}`;
     //  affichage bulle ressenti
-const divRessenti=document.querySelector('.ressenti');
 divDeRessenti=document.createElement('div');
 divDeRessenti.classList.add('ressenti');
 newSectionCard.appendChild(divDeRessenti);
 divDeRessenti.innerHTML=`${week.ressenti[i]}`;
-
      //  affichage des bulles du menu
-
-
+const navMenu=document.querySelector('nav');
+newHref=document.createElement('a');
+newHref.classList.add('button-circle');
+navMenu.appendChild(newHref);
+newHref.innerHTML=`week${i+1}`;
+newHref.setAttribute("href",`#week${i+1}`);
  }
-    
- }
+}
 affich();
  //création du formulaire d'ajout de semaine
 function addWeek(){
@@ -83,13 +83,6 @@ console.log("Etat bdd apres stockage:\n"+ week)
 
 // // suppression du formulaire
  })
-
-
 }
-// // appeler la fonction nouvelle semaine
-// const clickMenuAdd=document.querySelector('#add');
-
-// clickMenuAdd.addEventListener('click',function event () {
-//     console.log("ouverture formulaire d'ajout de semaine");
      addWeek();
-// });
+
